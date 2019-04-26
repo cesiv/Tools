b204ba5ef7abf83953406e2e1b6630cdb8105801
cesiv <361660500@qq.com>
2019-04-26 16:29:19 +0800

    Update InfoDAOImpl.java

--- src/main/java/com/jumper/pdf/dao/impl/InfoDAOImpl.java
+++ src/main/java/com/jumper/pdf/dao/impl/InfoDAOImpl.java
@@ -13,30 +13,12 @@ public class InfoDAOImpl extends BaseDAO implements InfoDAO {
 
     @Override
     public List<Map<Object, Object>> searchWomanInfoByCondition(QueryCondition queryCondition) {
-        return  super.query("SELECT u.user_id,u.age 年龄,u.last_period 末次月经,o.report_url 报告,o.report_time 解读时间,r.record_files 胎心文件,r.uterus_record 宫缩文件,r.fetal_move_value 胎动 FROM monitor_order_consumer o \n" +
-                "LEFT JOIN user_extra_info u ON o.user_id=u.user_id\n" +
-                "LEFT JOIN monitor_heartrate_record r ON o.record_id=r.id\n" +
-                "LEFT JOIN user_info ui ON ui.id=o.user_id\n" +
-                "WHERE o.is_auto=0 \n" +
-                "AND ui.mobile =?\n" +
-                "AND \n" +
-                "( (UNIX_TIMESTAMP(r.add_time) >= UNIX_TIMESTAMP(?)-10000 AND UNIX_TIMESTAMP(r.add_time)<=UNIX_TIMESTAMP(?)+10000)\n" +
-                "OR ( UNIX_TIMESTAMP(o.report_time) >= UNIX_TIMESTAMP(?)-10000 AND UNIX_TIMESTAMP(o.report_time)<=UNIX_TIMESTAMP(?)+10000)\n" +
-                ")",new Object[]{queryCondition.getMobile(),queryCondition.getQueryTime(),queryCondition.getQueryTime(),queryCondition.getQueryTime(),queryCondition.getQueryTime()});
+        return  super.query("")",new Object[]{queryCondition.getMobile(),queryCondition.getQueryTime(),queryCondition.getQueryTime(),queryCondition.getQueryTime(),queryCondition.getQueryTime()});
     }
 
     @Override
     public List<Map<Object, Object>> searchWomanInfoWithName(QueryCondition queryCondition) {
-        String sql ="SELECT u.user_id,u.age 年龄,u.last_period 末次月经,o.report_url 报告,o.report_time 解读时间,r.record_files 胎心文件,r.uterus_record 宫缩文件,r.fetal_move_value 胎动 FROM monitor_order_consumer o \n" +
-                "LEFT JOIN user_extra_info u ON o.user_id=u.user_id\n" +
-                "LEFT JOIN monitor_heartrate_record r ON o.record_id=r.id\n" +
-                "LEFT JOIN user_info ui ON ui.id=o.user_id\n" +
-                "WHERE o.is_auto=0 \n" +
-                "AND u.real_name =?\n" +
-                "AND \n" +
-                "( (UNIX_TIMESTAMP(r.add_time) >= UNIX_TIMESTAMP(?)-10000 AND UNIX_TIMESTAMP(r.add_time)<=UNIX_TIMESTAMP(?)+10000)\n" +
-                "OR ( UNIX_TIMESTAMP(o.report_time) >= UNIX_TIMESTAMP(?)-10000 AND UNIX_TIMESTAMP(o.report_time)<=UNIX_TIMESTAMP(?)+10000)\n" +
-                ")";
+        String sql ="";
 
         System.out.println("sql = [" + sql + "]");
         System.out.println("queryCondition = [" + JSON.toJSONString(queryCondition) + "]");
@@ -46,12 +28,7 @@ public class InfoDAOImpl extends BaseDAO implements InfoDAO {
     @Override
     public List<Map<Object, Object>> searchRecordInfoWithRecordID(QueryCondition queryCondition) {
 
-        String sql ="SELECT u.user_id,u.age 年龄,u.last_period 末次月经,o.report_url 报告,o.report_time 解读时间,r.record_files 胎心文件,r.uterus_record 宫缩文件,r.fetal_move_value 胎动 ,r.id  监测记录ID FROM monitor_order_consumer o \n" +
-                "LEFT JOIN user_extra_info u ON o.user_id=u.user_id\n" +
-                "LEFT JOIN monitor_heartrate_record r ON o.record_id=r.id\n" +
-                "LEFT JOIN user_info ui ON ui.id=o.user_id\n" +
-                "WHERE o.is_auto=0 \n" +
-                "AND r.id =?\n";
+        String sql ="";
         return super.query(sql,new Object[]{queryCondition.getRecordId()});
     }
 }
